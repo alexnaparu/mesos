b9738e8b9feba846b9c31efcfcb4b30964fe8a4b
Alex Naparu <alex.naparu@microsoft.com>
2015-11-18 13:05:59 -0800

    Windows: Fixed POSIX build break cause by chown changes.

--- 3rdparty/libprocess/3rdparty/stout/include/stout/posix/os.hpp
+++ 3rdparty/libprocess/3rdparty/stout/include/stout/posix/os.hpp
@@ -68,6 +68,7 @@
 #include <stout/os/exists.hpp>
 #include <stout/os/fcntl.hpp>
 #include <stout/os/fork.hpp>
+#include <stout/os/glob.hpp>
 #ifdef __linux__
 #include <stout/os/linux.hpp>
 #endif // __linux__
@@ -82,6 +83,7 @@
 #include <stout/os/sysctl.hpp>
 #endif // __APPLE__
 
+#include <stout/os/posix/chown.hpp>
 #include <stout/os/raw/environment.hpp>
 
 namespace os {
@@ -104,35 +106,6 @@ inline void setenv(const std::string& key,
 inline void unsetenv(const std::string& key)
 {
   ::unsetenv(key.c_str());
-}
-
-
-// Executes a command by calling "/bin/sh -c <command>", and returns
-// after the command has been completed. Returns 0 if succeeds, and
-// return -1 on error (e.g., fork/exec/waitpid failed). This function
-// is async signal safe. We return int instead of returning a Try
-// because Try involves 'new', which is not async signal safe.
-inline int system(const std::string& command)
-{
-  pid_t pid = ::fork();
-
-  if (pid == -1) {
-    return -1;
-  } else if (pid == 0) {
-    // In child process.
-    ::execlp("sh", "sh", "-c", command.c_str(), (char*) NULL);
-    ::exit(127);
-  } else {
-    // In parent process.
-    int status;
-    while (::waitpid(pid, &status, 0) == -1) {
-      if (errno != EINTR) {
-        return -1;
-      }
-    }
-
-    return status;
-  }
 }
 
 
