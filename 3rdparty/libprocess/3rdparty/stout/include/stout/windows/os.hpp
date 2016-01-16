57b4cbddc886ad9f3e5c7b8d1baeb8f3d219fd4a
Alex Naparu <alex.naparu@microsoft.com>
2016-01-15 18:02:24 -0800

    Windows: Implemented os::sleep(), os::cpus(), os::memory() and os::uname().

--- 3rdparty/libprocess/3rdparty/stout/include/stout/windows/os.hpp
+++ 3rdparty/libprocess/3rdparty/stout/include/stout/windows/os.hpp
@@ -15,6 +15,7 @@
 
 #include <direct.h>
 #include <io.h>
+#include <psapi.h>
 
 #include <sys/utime.h>
 
@@ -31,6 +32,7 @@
 #include <stout/try.hpp>
 #include <stout/windows.hpp>
 
+#include <stout/os/os.hpp>
 #include <stout/os/raw/environment.hpp>
 
 namespace os {
@@ -132,29 +134,23 @@ inline Result<std::string> user(Option<uid_t> uid = None())
 {
   UNIMPLEMENTED;
 }
-
-
+*/
 // Suspends execution for the given duration.
 inline Try<Nothing> sleep(const Duration& duration)
 {
-  UNIMPLEMENTED;
+  DWORD milliseconds = static_cast<DWORD>(duration.ms());
+  ::Sleep(milliseconds);
 }
-
-
-// Returns the list of files that match the given (shell) pattern.
-inline Try<std::list<std::string>> glob(const std::string& pattern)
-{
-  UNIMPLEMENTED;
-}
-
 
 // Returns the total number of cpus (cores).
 inline Try<long> cpus()
 {
-  UNIMPLEMENTED;
+  SYSTEM_INFO sysInfo;
+  ::GetSystemInfo(&sysInfo);
+  return static_cast<long>(sysInfo.dwNumberOfProcessors);
 }
 
-
+/*
 // Returns load struct with average system loads for the last
 // 1, 5 and 15 minutes respectively.
 // Load values should be interpreted as usual average loads from
@@ -163,36 +159,143 @@ inline Try<Load> loadavg()
 {
   UNIMPLEMENTED;
 }
-
+*/
 
 // Returns the total size of main and free memory.
 inline Try<Memory> memory()
 {
-  UNIMPLEMENTED;
-}
+  Memory memory;
 
+  MEMORYSTATUSEX memoryStatus;
+  memoryStatus.dwLength = sizeof(MEMORYSTATUSEX);
+  if (!GlobalMemoryStatusEx(&memoryStatus)) {
+    return WindowsError("`memory()`: Could not call `GlobalMemoryStatusEx`");
+  }
+
+  memory.total = Bytes(memoryStatus.ullTotalPhys);
+  memory.free = Bytes(memoryStatus.ullAvailPhys);
+  memory.totalSwap = Bytes(memoryStatus.ullTotalPageFile);
+  memory.freeSwap = Bytes(memoryStatus.ullAvailPageFile);
+
+  return memory;
+}
 
 // Return the system information.
 inline Try<UTSInfo> uname()
 {
-  UNIMPLEMENTED;
+  UTSInfo info;
+
+  OSVERSIONINFOEX osVersion;
+  osVersion.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+  if (!::GetVersionEx((LPOSVERSIONINFO)&osVersion)) {
+    return WindowsError("`os::uname()`: Failed to call `GetVersionEx`");
+  }
+
+  switch(osVersion.wProductType) {
+    case VER_NT_DOMAIN_CONTROLLER:
+    case VER_NT_SERVER:
+      info.sysname = "Windows Server";
+      break;
+    default:
+      info.sysname = "Windows";
+  }
+
+  info.release = std::to_string(osVersion.dwMajorVersion) + "." +
+      std::to_string(osVersion.dwMinorVersion);
+  info.version = std::to_string(osVersion.dwBuildNumber);
+  if (osVersion.szCSDVersion[0] != '\0') {
+    info.version.append(" ");
+    info.version.append(osVersion.szCSDVersion);
+  }
+
+  // Get DNS name of the local computer. First, find the size of the output
+  // buffer.
+  DWORD size = 0;
+  if (!::GetComputerNameEx(ComputerNameDnsHostname, NULL, &size) &&
+      ::GetLastError() != ERROR_MORE_DATA) {
+    return WindowsError("`os::uname()`: Failed to call `GetComputerNameEx`");
+  }
+
+  std::shared_ptr<char> computerName(
+      (char *)malloc((size + 1) * sizeof(char)));
+
+  if (!::GetComputerNameEx(ComputerNameDnsHostname, computerName.get(),
+      &size)) {
+    return WindowsError("`os::uname()`: Failed to call `GetComputerNameEx`");
+  }
+
+  info.nodename = computerName.get();
+
+  // Get OS architecture
+  SYSTEM_INFO systemInfo;
+  ::GetNativeSystemInfo(&systemInfo);
+  switch(systemInfo.wProcessorArchitecture) {
+    case PROCESSOR_ARCHITECTURE_AMD64:
+      info.machine = "AMD64";
+      break;
+    case PROCESSOR_ARCHITECTURE_ARM:
+      info.machine = "ARM";
+      break;
+    case PROCESSOR_ARCHITECTURE_IA64:
+      info.machine = "IA64";
+      break;
+    case PROCESSOR_ARCHITECTURE_INTEL:
+      info.machine = "x86";
+      break;
+    default:
+      info.machine = "Unknown";
+  }
+
+  return info;
 }
 
-
+/*
 inline Try<std::list<Process>> processes()
 {
   UNIMPLEMENTED;
 }
-
+*/
 
 // Overload of os::pids for filtering by groups and sessions.
 // A group / session id of 0 will fitler on the group / session ID
 // of the calling process.
 inline Try<std::set<pid_t>> pids(Option<pid_t> group, Option<pid_t> session)
 {
-  UNIMPLEMENTED;
+  // Windows does not have the concept of a process group, so we need to
+  // enumerate all processes.
+  //
+  // The list of processes might differ between calls, so continue calling
+  // `EnumProcesses` until the output buffer is large enough. The call is
+  // considered to fully succeed when the function returns non-zero and the
+  // number of bytes returned is less than the size of the `pids` array. If
+  // that's not the case, then we need to increase the size of the `pids` array
+  // and attempt the call again.
+  //
+  // To minimize the number of calls (at the expense
+  // or memory), we choose to allocate double the amount suggested by
+  // `EnumProcesses`.
+  DWORD *pids = NULL;
+  DWORD bytes = 1024;
+  DWORD pidsSize = 0;
+
+  // TODO(alexnaparu): Set a limit to the memory that can be used.
+  while (pidsSize <= bytes) {
+    pidsSize = 2 * bytes;
+    pids = (DWORD *)realloc(pids, pidsSize);
+    if (!::EnumProcesses(pids, pidsSize, &bytes)) {
+      free(pids);
+      return WindowsError("`os::pids()`: Failed to call `EnumProcesses`");
+    }
+  }
+
+  std::set<pid_t> result;
+  for (DWORD i = 0; i < bytes / sizeof(DWORD); i++) {
+    result.insert(pids[i]);
+  }
+
+  free(pids);
+  return result;
 }
-*/
 } // namespace os {
 
 #endif // __STOUT_WINDOWS_OS_HPP__
